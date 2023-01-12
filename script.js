6cbfe6df56016f00145bb220da41fed2b213c33c
Rupkumar Sarkar <109466063+rup52333@users.noreply.github.com>
2023-01-12 09:26:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function threeSum(arr, target) {
   //your code here
+   arr.sort((a,b)=>a-b);
+  const ans=arr[0]+arr[1]+arr[2];
+for(let i=0;i<arr.length;i+){
+ let start=i+1;
+let end=arr.length-1;
+while(start<end){
+	const sum=arr[i]+arr[start]+arr[end];
+if(sum<target){
+	start++;
+}
+else if(sum>target){
+	end--;
+}
+else if(Math.abs(sum-target)<Math.abs(ans-target)){
+	ans=sum;
+}
+	
+}
+
+	
+}
+
+return ans;
+	
 }
 
 module.exports = threeSum;
